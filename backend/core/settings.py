23f714c199a25cc46fc29edcd42f727ccf99eaa9
elcapitan88 <alexh6788@gmail.com>
2024-11-30 23:03:22 -0600

    Remove static files configuration - using separate frontend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -60,5 +59,5 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
