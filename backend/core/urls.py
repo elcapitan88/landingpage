1825b052d7c0b46c850436dc72abfacf78f849b2
elcapitan88 <alexh6788@gmail.com>
2024-11-30 23:21:34 -0600

    Remove staticfiles and add health check endpoints

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 def health_check(request):
@@ -8,4 +8,5 @@ def health_check(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', health_check),
+    path('health/', health_check),
 ]
