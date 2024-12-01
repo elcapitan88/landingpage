b284f977c37d1cedd3e64ac41cebef05c59f110a
elcapitan88 <alexh6788@gmail.com>
2024-12-01 16:09:04 -0600

    merge: resolve conflicts

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,16 +1,5 @@
 import os
-import sys
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Add the project directory to the Python path
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
